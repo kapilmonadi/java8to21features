ade577d48a6ca5fe21bb0325b33c6ccd199470e7
kaplimonadi <kapilmonadi@gmail.com>
2025-10-10 11:29:17 +0530

    added sleep

--- src/main/java/com/kapil/concurrency/SampleThreadWithRunnable.java
+++ src/main/java/com/kapil/concurrency/SampleThreadWithRunnable.java
@@ -7,10 +7,11 @@ public class SampleThreadWithRunnable implements Runnable {
         System.out.println("Executing thread : " + Thread.currentThread().getName());
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         System.out.println("Executing thread : " + Thread.currentThread().getName());
         SampleThreadWithRunnable sampleThreadWithRunnable = new SampleThreadWithRunnable();
         Thread newThread = new Thread(sampleThreadWithRunnable);
         newThread.start();
+        Thread.sleep(2000);
     }
 }
