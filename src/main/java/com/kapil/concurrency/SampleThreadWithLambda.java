0ec15dbfe28db2a0fe2b6ba091366eb7a5da3f88
kaplimonadi <kapilmonadi@gmail.com>
2025-10-10 20:27:12 +0530

    sample class

---
+++ src/main/java/com/kapil/concurrency/SampleThreadWithLambda.java
@@ -0,0 +1,11 @@
+package com.kapil.concurrency;
+
+public class SampleThreadWithLambda {
+    public static void main(String[] args) {
+        System.out.println("Executing thread : " + Thread.currentThread().getName());
+        Thread newThread = new Thread(() -> {
+            System.out.println("Executing thread : " + Thread.currentThread().getName());
+        });
+        newThread.start();
+    }
+}
