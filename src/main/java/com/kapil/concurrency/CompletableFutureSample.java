716e462284179feb9099f1b07fadd96fdeba1daf
kaplimonadi <kapilmonadi@gmail.com>
2025-10-16 11:58:29 +0530

    added more example

--- src/main/java/com/kapil/concurrency/CompletableFutureSample.java
+++ src/main/java/com/kapil/concurrency/CompletableFutureSample.java
@@ -18,6 +18,14 @@ public class CompletableFutureSample {
         asyncTaskCF.get();
         System.out.println("Async task completed");
 
+        var completableFutureForDescription = getProductDescriptionWithCompleteCompletableFutureTask();
+
+        // print the value received from the CF, should return the string "No description available"
+        // this is a non blocking call so the main thread will continue its execution
+        completableFutureForDescription.thenAccept(System.out::println);
+
+        System.out.println("Executing after the call to get the description of the Product");
+
         var completableFutureProductDetails = CompletableFuture.supplyAsync(() -> {
             try {
                 Thread.sleep(Duration.ofSeconds(1));
@@ -73,5 +81,31 @@ public class CompletableFutureSample {
             }
             System.out.println("Task completed !");
         });
+    }
+
+    // The description service is simulated to take at least 5 seconds
+    // we create a separate thread that can return the default description i.e. No description available after 2 seconds
+    private static CompletableFuture<String> getProductDescriptionWithCompleteCompletableFutureTask() {
+        CompletableFuture<String> stringCompletableFuture = CompletableFuture.supplyAsync(() -> {
+            // Fetch the description by calling a service,
+            // if the service does not return back in stipulated time return a default description
+            try {
+                Thread.sleep(Duration.ofSeconds(5));
+                // simulating that the service call did not return and returning default value after 2 mins
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+            return "Great Brain Teaser";
+        });
+        Thread.ofPlatform().start(() -> {
+            try {
+                Thread.sleep(Duration.ofSeconds(2));
+                // simulating that the service call did not return and returning default value after 2 mins
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+            stringCompletableFuture.complete("No description available");
+        });
+        return stringCompletableFuture;
     }
 }
