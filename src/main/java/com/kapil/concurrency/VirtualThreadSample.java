3387569c5c07bf2b6c0b117e695dbd8b95d13c1d
kaplimonadi <kapilmonadi@gmail.com>
2025-10-17 12:39:42 +0530

    example on how to create a virtual thread

--- src/main/java/com/kapil/concurrency/VirtualThreadSample.java
+++ src/main/java/com/kapil/concurrency/VirtualThreadSample.java
@@ -1,4 +1,25 @@
 package com.kapil.concurrency;
 
+import java.time.Duration;
+
 public class VirtualThreadSample {
+    public static void main(String[] args) throws InterruptedException {
+        runPlatformThread();
+        runVirtualThread();
+        Thread.sleep(Duration.ofSeconds(2));
+    }
+
+    private static void runPlatformThread() {
+        var platformThread = Thread.ofPlatform().unstarted(() -> {
+            System.out.println("I'm bering executed by a Platform Thread");
+        });
+        platformThread.start();
+    }
+
+    private static void runVirtualThread() {
+        var platformThread = Thread.ofVirtual().unstarted(() -> {
+            System.out.println("I'm bering executed by a Virtual Thread");
+        });
+        platformThread.start();
+    }
 }
