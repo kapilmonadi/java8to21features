e165d8a3fdef2de3b7c2a7d76d457f168547e4e9
kaplimonadi <kapilmonadi@gmail.com>
2025-10-20 14:09:58 +0530

    added sample for Fixed thread pool Executor Service

--- src/main/java/com/kapil/concurrency/ExecutorServiceSample.java
+++ src/main/java/com/kapil/concurrency/ExecutorServiceSample.java
@@ -6,14 +6,38 @@ public class ExecutorServiceSample {
     public static void main(String[] args) {
         // creating a simple Executor Service
 
-        // Let's start with a single thread executor
-
         System.out.println("I'm the main thread");
         System.out.println(Thread.currentThread().getName());
 
+        // Let's start with a single thread executor
+        runSingleThreadExecutor();
+
+        // run multiple threads executor
+        runMultipleThreadExecutor();
+    }
+
+    private static void runSingleThreadExecutor() {
         try (var executorService = Executors.newSingleThreadExecutor()) {
             executorService.submit(() -> {
                 System.out.println("I'm running in a single thread started by the Executor Service");
+                System.out.println("Thread name is : " + Thread.currentThread().getName());
+            });
+        }
+        finally{
+            System.out.println("Done using the Executor Service");
+        }
+    }
+
+    private static void runMultipleThreadExecutor() {
+        try (var executorService = Executors.newFixedThreadPool(5)) {
+            executorService.submit(() -> {
+                System.out.println("I'm task 1");
+                System.out.println("I'm running in a thread pool executor of 5 threads started by the Executor Service");
+                System.out.println("Thread name is : " + Thread.currentThread().getName());
+            });
+            executorService.submit(() -> {
+                System.out.println("I'm task 2");
+                System.out.println("I'm running in a thread pool executor of 5 threads started by the Executor Service");
                 System.out.println("Thread name is : " + Thread.currentThread().getName());
             });
         }
