85ab795d57213f4fcfdbc8ca4fbf14d14e04fcef
kaplimonadi <kapilmonadi@gmail.com>
2025-10-22 19:19:45 +0530

    added example for using synchronized

---
+++ src/main/java/com/kapil/concurrency/SynchronizedSample.java
@@ -0,0 +1,34 @@
+package com.kapil.concurrency;
+
+public class SynchronizedSample {
+
+    private static String name;
+
+    public static void main(String[] args) {
+
+        System.out.println("Name is " + name);
+        name = "Main";
+        System.out.println("Name after assignment in main thread is " + name);
+        System.out.println("Calling threads that will modify the name");
+        modifyName();
+    }
+
+    private static void modifyName() {
+        var thread1Str = "Thread1";
+        var thread2Str = "Thread2";
+
+        var platformThread1 = Thread.ofPlatform().name(thread1Str).start(() -> {
+            System.out.println("I will modify the name to Thread 1");
+            updateName(thread1Str);
+        });
+        var platformThread2 = Thread.ofPlatform().name(thread2Str).start(() -> {
+            System.out.println("I will modify the name to Thread 2");
+            updateName(thread2Str);
+        });
+    }
+
+    private synchronized static void updateName(String newName) {
+        name = newName;
+        System.out.println("The new updated name is " + name + " and it was updated by " + Thread.currentThread().getName());
+    }
+}
