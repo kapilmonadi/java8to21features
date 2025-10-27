1354c0174a7cb50e3e62589f8757fddb2a18b106
kaplimonadi <kapilmonadi@gmail.com>
2025-10-27 17:33:42 +0530

    added sample for synchronization

--- src/main/java/com/kapil/concurrency/SynchronizedSample.java
+++ src/main/java/com/kapil/concurrency/SynchronizedSample.java
@@ -3,7 +3,7 @@ package com.kapil.concurrency;
 public class SynchronizedSample {
 
     private static String name;
-
+    private static final String LOCK_STR = "lockString";
     public static void main(String[] args) {
 
         System.out.println("Name is " + name);
@@ -21,19 +21,23 @@ public class SynchronizedSample {
             System.out.println("I will modify the name to Thread 1");
             updateName(thread1Str);
             updateNameSyncBlock(thread1Str);
+            updateNameSyncInstance(thread1Str);
         });
         var platformThread2 = Thread.ofPlatform().name(thread2Str).start(() -> {
             System.out.println("I will modify the name to Thread 2");
             updateName(thread2Str);
             updateNameSyncBlock(thread2Str);
+            updateNameSyncInstance(thread2Str);
         });
     }
 
+    // synchronizing on the method
     private synchronized static void updateName(String newName) {
         name = newName;
         System.out.println("The new updated name is " + name + " and it was updated by " + Thread.currentThread().getName());
     }
 
+    // synchronizing on the class object
     private static void updateNameSyncBlock(String newName) {
         synchronized (SynchronizedSample.class){
             name = newName;
@@ -41,4 +45,11 @@ public class SynchronizedSample {
         }
     }
 
+    // synchronizing on an object (String object in the current example)
+    private static void updateNameSyncInstance(String newName) {
+        synchronized (LOCK_STR){
+            name = newName;
+            System.out.println("The new updated name is " + name + " and it was updated by " + Thread.currentThread().getName());
+        }
+    }
 }
