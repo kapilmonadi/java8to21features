a879bffeb1beb3cff23cb28e9b57f72647da45d3
kaplimonadi <kapilmonadi@gmail.com>
2026-01-01 22:41:41 +0530

    example for volatile

---
+++ src/main/java/com/kapil/concurrency/VolatileSample.java
@@ -0,0 +1,22 @@
+package com.kapil.concurrency;
+
+public class VolatileSample {
+
+    private static volatile boolean isActive = false;
+
+    public static void main(String[] args) {
+        runThread1();
+        runThread2();
+    }
+
+    private static void runThread1(){
+
+    }
+
+    private static void runThread2(){
+
+    }
+
+
+
+}
